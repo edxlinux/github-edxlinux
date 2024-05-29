0ba0d47fcd6f51c3613460d4af8cb316cae8bbbf
edxlinux <maximoffluis1@gmail.com>
2024-05-29 03:54:00 -0300

    cvscript.sh
    este script instala o cubic e o virualbox execute o script como super-usuario digite sua senha
    e depis digite ./cvscript.sh

---
+++ cvscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt-add-repository ppa:cubic-wizard/release
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6494C6D6997C215E
+sudo apt update && sudo apt install cubic
+
+sudo apt install virtualbox
+
+
+echo "o grupo edx linux agradece"
+
